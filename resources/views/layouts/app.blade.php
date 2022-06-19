ed863a44866975237fff86df01e0ae1994c69166
Hassan Alkhalidi <gruceing@gmail.com>
2022-06-19 22:34:21 +0300

    as

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" href="{{ asset('img/a.png') }}" type="image/x-icon">
+    {{-- <link rel="icon" href="{{ asset('img/a.png') }}" type="image/x-icon"> --}}
     {{-- Fonts --}}
 
     {{--
