ded23d0558914b54bdf5447c396c594abfcd6cd3
zal3 <zainabamjedlatef31@gmail.com>
2022-06-20 18:19:50 +0300

    a

--- resources/views/livewire/task/card.blade.php
+++ resources/views/livewire/task/card.blade.php
@@ -21,7 +21,7 @@
                             class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-black">Edit</a>
                     </li>
                     <li>
-                        <a href="#"
+                         <a href="#"
                             class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-black">Export
                             Data</a>
                     </li>
