d9f9cf54a0082296ef1d466cd316068a3151b604
zal3 <zainabamjedlatef31@gmail.com>
2022-06-21 02:43:18 +0300

    a

--- app/Http/Livewire/Task/Add.php
+++ app/Http/Livewire/Task/Add.php
@@ -21,19 +21,18 @@ class Add extends Component
         'end_at' => 'required',
     ];
 
-    public function add( ){
+    public function add( Task $task ){
         $this->validate();
 
-        $task = Task::create($this->task);
+        $data = [
+            'title' => $this->title,
+            'description' => $this->description,
+            'importance' => $this->importance,
+            'start_at' => $this->start_at,
+            'end_at' => $this->end_at,
+        ];
+        $task = Task::create($data);
 
-        $this->emitTo('task.index' , '$refresh');
-        $this->alert('success', __('ui.data_has_been_add_successfully'), [
-            'position' => 'top',
-            'timer' => 3000,
-            'toast' => true,
-            'timerProgressBar' => true,
-            'width' => '400',
-        ]);
         if(count($this->files) > 0)
             foreach($this->files as $file){
                 $new_file = $task->files()->create([
@@ -43,7 +42,6 @@ class Add extends Component
             }
 
             $this->reset();
-            $this->emitTo('task.index' , '$refresh');
         $this->alert('success', __('ui.data_has_been_add_successfully'), [
             'position' => 'top',
             'timer' => 3000,
--- resources/views/livewire/task/add.blade.php
+++ resources/views/livewire/task/add.blade.php
@@ -13,7 +13,6 @@
                         <input wire:model="title" type="text"
                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             placeholder={{ __('ui.title') }} required>
-
                     </div>
                     {{-- <div>
                         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
@@ -41,7 +40,7 @@
                                         clip-rule="evenodd"></path>
                                 </svg>
                             </div>
-                            <input datepicker="" type="text" wire:model="start_at"
+                            <input datepicker=""  wire:model="start_at"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input"
                                 placeholder="Select date">
                         </div>
