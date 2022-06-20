3847ed359d84ad1f29ee3320a2c820aa897cd047
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-06-20 03:54:20 +0300

    add

--- app/Http/Livewire/Employee/Add.php
+++ app/Http/Livewire/Employee/Add.php
@@ -3,9 +3,27 @@
 namespace App\Http\Livewire\Employee;
 
 use Livewire\Component;
+use App\Models\Employee;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+
 
 class Add extends Component
 {
+    use LivewireAlert;
+    public $employee;
+
+    protected $rules = [
+        'employee.name' => 'required',
+    ];
+
+    public function save()
+    {
+        $this->validate();
+        Employee::create($this->employee);
+
+        $this->emitTo('employee.index', '$refresh');
+    }
+
     public function render()
     {
         return view('livewire.employee.add');
