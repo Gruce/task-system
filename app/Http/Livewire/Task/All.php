292f9bf9bec73cbea6a3c5da405a8af8902331de
hmh_6 <www.hussamcr7@gmail.com>
2022-06-21 14:40:09 +0300

    u

--- app/Http/Livewire/Task/All.php
+++ app/Http/Livewire/Task/All.php
@@ -8,12 +8,10 @@ use App\Models\Project;
 class All extends Component
 {
 
+    public $state = 1;
 
-    public function render()
-    {
-
-        $tasks  = Task::with('project')->get();
-
+    public function render(){
+        $tasks1  = Task::with('project')->get();
         return view('livewire.task.all', compact('tasks'));
     }
 }
