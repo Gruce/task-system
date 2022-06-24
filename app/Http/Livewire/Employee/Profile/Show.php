990e1df190f862d34bbea1e512cf57b4e32caf19
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-06-25 02:54:32 +0300

    add

--- app/Http/Livewire/Employee/Profile/Show.php
+++ app/Http/Livewire/Employee/Profile/Show.php
@@ -6,11 +6,13 @@ use App\Models\Employee;
 use Livewire\Component;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use App\Models\Task;
+use App\Models\User;
 use Carbon\Carbon;
+
 class Show extends Component
 {
     use LivewireAlert;
-     public $state ;
+    public $state;
 
     protected $rules = [
         'employee.user.name' => 'required',
@@ -26,27 +28,23 @@ class Show extends Component
         $employee->state = !$employee->state;
         $employee->save();
         $msg = $employee->state ? 'ui.the_account_has_been_disabled' : 'ui.the_account_has_been_activated';
-        $this->alert('success', __($msg),
-        [
-            'position' => 'top',
-            'timer' => 3000,
-            'toast' => true,
-            'timerProgressBar' => true,
-            'width' => '400',
-        ]);
+        $this->alert(
+            'success',
+            __($msg),
+            [
+                'position' => 'top',
+                'timer' => 3000,
+                'toast' => true,
+                'timerProgressBar' => true,
+                'width' => '400',
+            ]
+        );
     }
 
-    public function changeGander(Employee $employee)
+    public function change_gender(User $user)
     {
-        dg($employee->user->gender);
-        if($employee->user->gender == 1){
-            $employee->user->gender = 2;
-            $employee->save();
-        }else{
-            $employee->user->gender = 1;
-            $employee->save();
-        }
-
+        $user->gender == 1 ? $user->gender = 2 : $user->gender = 1;
+        $user->save();
     }
     public function mount($id)
     {
@@ -54,25 +52,26 @@ class Show extends Component
             ->withCount(['tasks', 'files'])
             ->findOrFail($id);
         //  dd($this->employee);
-            // dd($res);
+        // dd($res);
     }
-    public function calctimeprogress()
+    // public function calctimeprogress()
+    // {
+    //     $this->task = Task::get()->last();
+    //     $task=$this->task;
+    //     $start_at = $task->start_at;
+
+    //        $s = strtotime($start_at) ;
+    //        $end_at = $task->end_at;
+    //        $e = strtotime($end_at);
+    //        $t1 = strtotime($end_at)- strtotime(now());
+    //        $t2 = strtotime($end_at)- strtotime($start_at);
+    //        $res = 100 - ($t1/$t2)*100;
+
+    //     return (int)$res;
+    //     }
+
+    public function edit_name()
     {
-        $this->task = Task::get()->last();
-        $task=$this->task;
-        $start_at = $task->start_at;
-
-           $s = strtotime($start_at) ;
-           $end_at = $task->end_at;
-           $e = strtotime($end_at);
-           $t1 = strtotime($end_at)- strtotime(now());
-           $t2 = strtotime($end_at)- strtotime($start_at);
-           $res = 100 - ($t1/$t2)*100;
-
-        return (int)$res;
-        }
-
-    public function edit_name(){
         $this->employee->save();
         $this->alert('success', __('ui.data_has_been_edited_successfully'), [
             'position' => 'top',
@@ -84,7 +83,7 @@ class Show extends Component
     }
     public function render()
     {
-        $this->progress=$this->calctimeprogress();
+        // $this->progress = $this->calctimeprogress();
         return view('livewire.employee.profile.show');
     }
 }
--- resources/views/livewire/employee/profile/show.blade.php
+++ resources/views/livewire/employee/profile/show.blade.php
@@ -10,19 +10,12 @@
                                 <h5 x-show="!name" class="text-xl font-bold tracking-tight text-secondary-700">
                                     {{$employee->name}}
                                 </h5>
-                                <div class="flex items-center gap-4" @click.stop=""
-                                    @click.outside="name = false">
-                                    <input wire:keydown.enter="edit_name" wire:model="employee.user.name" type="text"
-                                        x-show="name"
-                                        class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                                        required>
+                                <div class="flex items-center gap-4" @click.stop="" @click.outside="name = false">
+                                    <input wire:keydown.enter="edit_name" wire:model="employee.user.name" type="text" x-show="name" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" required>
 
-                                    <div
-                                        class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
-                                        <a @click="name = !name" x-show="!name" href="#"><i
-                                                class="fas fa-pen"></i></a>
-                                        <a wire:click="edit_name" @click="name = !name" x-show="name" href="#"><i
-                                                class="fas fa-check"></i></a>
+                                    <div class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
+                                        <a @click="name = !name" x-show="!name" href="#"><i class="fas fa-pen"></i></a>
+                                        <a wire:click="edit_name" @click="name = !name" x-show="name" href="#"><i class="fas fa-check"></i></a>
                                     </div>
                                 </div>
                                 {{-- <div class="flex items-center gap-4" @click.stop="" @click.outside="name = false">
@@ -36,22 +29,16 @@
                             </div>
 
                         </div>
-                         <div class="flex items-center gap-4  text-secondary-600 group">
+                        <div class="flex items-center gap-4  text-secondary-600 group">
                             <h6 x-show="!job" class="text-sm  font-bold tracking-tight text-secondary-700">
                                 {{$employee->job}}
                             </h6>
                             <div class="flex items-center gap-4" @click.stop="" @click.outside="job = false">
-                                <input wire:keydown.enter="edit_name" wire:model="employee.job" type="text"
-                                    x-show="job"
-                                    class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                                    required>
+                                <input wire:keydown.enter="edit_name" wire:model="employee.job" type="text" x-show="job" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" required>
 
-                                <div
-                                    class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
-                                    <a @click="job = !job" x-show="!job" href="#"><i
-                                            class="fas fa-pen"></i></a>
-                                    <a wire:click="edit_name" @click="job = !job" x-show="job" href="#"><i
-                                            class="fas fa-check"></i></a>
+                                <div class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
+                                    <a @click="job = !job" x-show="!job" href="#"><i class="fas fa-pen"></i></a>
+                                    <a wire:click="edit_name" @click="job = !job" x-show="job" href="#"><i class="fas fa-check"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -61,17 +48,11 @@
                                 {{$employee->email}}
                             </h6>
                             <div class="flex items-center gap-4" @click.stop="" @click.outside="email = false">
-                                <input wire:keydown.enter="edit_name" wire:model="employee.user.email" type="text"
-                                    x-show="email"
-                                    class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                                    required>
+                                <input wire:keydown.enter="edit_name" wire:model="employee.user.email" type="text" x-show="email" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" required>
 
-                                <div
-                                    class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
-                                    <a @click="email = !email" x-show="!email" href="#"><i
-                                            class="fas fa-pen"></i></a>
-                                    <a wire:click="edit_name" @click="email = !email" x-show="email" href="#"><i
-                                            class="fas fa-check"></i></a>
+                                <div class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
+                                    <a @click="email = !email" x-show="!email" href="#"><i class="fas fa-pen"></i></a>
+                                    <a wire:click="edit_name" @click="email = !email" x-show="email" href="#"><i class="fas fa-check"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -81,38 +62,31 @@
                                 {{$employee->user->phonenumber}}
                             </h6>
                             <div class="flex items-center gap-4" @click.stop="" @click.outside="phone = false">
-                                <input wire:keydown.enter="edit_name" wire:model="employee.user.phonenumber" type="text"
-                                    x-show="phone"
-                                    class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                                    required>
+                                <input wire:keydown.enter="edit_name" wire:model="employee.user.phonenumber" type="text" x-show="phone" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" required>
 
-                                <div
-                                    class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
-                                    <a @click="phone = !phone" x-show="!phone" href="#"><i
-                                            class="fas fa-pen"></i></a>
-                                    <a wire:click="edit_name" @click="phone = !phone" x-show="phone" href="#"><i
-                                            class="fas fa-check"></i></a>
+                                <div class="px-2 py-1 duration-200 ease-in-out delay-100 rounded-lg opacity-0 group-hover:opacity-100 hover:text-secondary-800 bg-secondary-50">
+                                    <a @click="phone = !phone" x-show="!phone" href="#"><i class="fas fa-pen"></i></a>
+                                    <a wire:click="edit_name" @click="phone = !phone" x-show="phone" href="#"><i class="fas fa-check"></i></a>
                                 </div>
                             </div>
                         </div>
                         <div class="flex justify-center rounded-md shadow-sm gap-2 w-full" role="group">
-                           <button wire:click="changeGander({{$employee->id}})">
-                            @if($employee->user->gender==1)
-                            <i class="fas fa-2x fa-male"></i>
-                            @else
-                            <i class="fas fa-2x fa-female"></i>
-                            @endif
-                           </button>
+                            <button wire:click="change_gender({{ $employee->user->id }})">
+
+                                @if($employee->user->gender == 2)
+                                <i class="fas fa-2x fa-female"></i>
+                                @else
+                                <i class="fas fa-2x fa-male"></i>
+                                @endif
+                            </button>
                         </div>
                     </div>
                     {{-- <div>
                         <livewire:livewire-line-chart key="{{ $lineChartModel->reactiveKey() }}" :line-chart-model="$lineChartModel" />
                     </div> --}}
                     <div class="relative">
-                        <img class="w-20 h-20 mb-3 rounded-full" src="{{ $employee->user->profile_photo }}"
-                            alt="">
-                            <button wire:click="state({{$employee->id}})"
-                            @if($employee->state)
+                        <img class="w-20 h-20 mb-3 rounded-full" src="{{ $employee->user->profile_photo }}" alt="">
+                        <button wire:click="state({{$employee->id}})" @if($employee->state)
                             <i class="fa-solid fa-lock-open top-0 left-13 absolute text-green-700"></i>
                             @else
                             <i class="fa-solid fa-lock top-0 left-13 absolute text-red-600"></i>
@@ -122,8 +96,7 @@
                         {{-- <button type="button" class="bottom-0 left-7 bg-black    absolute  w-5.5 h-5.5  text-sm  text-center inline-flex items-center mr-2 ">
                             <i class="fa-solid fa-camera-rotate bg-black text-gray-600 text-lg"></i>
                         </button> --}}
-                        <button type="button"
-                            class="absolute  bottom-1  left-9  w-5.5 h-5.5 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-lg p-1 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        <button type="button" class="absolute  bottom-1  left-9  w-5.5 h-5.5 text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-full text-lg p-1 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <i class="fa-solid fa-camera-rotate  text-l"></i>
                         </button>
 
@@ -131,8 +104,7 @@
                 </div>
                 <div class="flex flex-col">
                     <div>
-                        <img src="https://www.microsoft.com/en-us/microsoft-365/blog/uploads/sites/2/2012/06/Excel-charts-11.png"
-                            alt="">
+                        <img src="https://www.microsoft.com/en-us/microsoft-365/blog/uploads/sites/2/2012/06/Excel-charts-11.png" alt="">
                     </div>
                     <div>
                         <img src="https://i.stack.imgur.com/aQMp3.png" alt="">
@@ -153,11 +125,11 @@
                             </div>
 
                             <div class="flex justify-between mb-1">
-                                <span class="text-xs font-medium text-secondary-500">{{$progress}}%</span>
+                                <span class="text-xs font-medium text-secondary-500">234234%</span>
                                 <span class="text-xs font-medium text-secondary-500">545</span>
                             </div>
                             <div class="w-full bg-gray-200 rounded-full h-1.5">
-                                <div class="bg-secondary-600 h-1.5 rounded-full" style="width: {{$progress}}%"></div>
+                                <div class="bg-secondary-600 h-1.5 rounded-full" style="width: 23%"></div>
                             </div>
                         </div>
                     </div>
@@ -180,11 +152,9 @@
                             </div>
                         </div>
                         <div class="flex justify-between mt-4 text-sm text-center text-gray-500">
-                            <div @class([
-                                'px- py-2 basis-1/2 flex items-center  justify-center gap-2',
-                                'border-r' => en(),
+                            <div @class([ 'px- py-2 basis-1/2 flex items-center  justify-center gap-2' , 'border-r'=> en(),
                                 'border-l' => ar(),
-                            ]) class="">
+                                ]) class="">
                                 <span class="text-2xl">45</span>
                             </div>
 
@@ -204,29 +174,21 @@
                         </h1>
                         <div class="relative ">
                             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor"
-                                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                        clip-rule="evenodd"></path>
+                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
                                 </svg>
                             </div>
-                            <input datepicker="" datepicker-autohide="" type="text"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input"
-                                placeholder="Select date">
+                            <input datepicker="" datepicker-autohide="" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="Select date">
                         </div>
 
                     </div>
                     <div class="flex flex-col  px-2 m-1 overflow-y-auto h-tasklist">
                         <ol class="relative border-l border-gray-300 ">
-                            <div
-                                class="p-4 w-full mb-5  bg-gray-100 rounded-lg  sm:p-4 dark:bg-gray-800 dark:border-gray-700">
+                            <div class="p-4 w-full mb-5  bg-gray-100 rounded-lg  sm:p-4 dark:bg-gray-800 dark:border-gray-700">
                                 <li class="mb-10 ml-4">
-                                    <div
-                                        class="absolute w-3 h-3 bg-black rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                                    <div class="absolute w-3 h-3 bg-black rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                                     </div>
-                                    <time
-                                        class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March
+                                    <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March
                                         2022</time>
                                     <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design
                                         in
@@ -239,14 +201,11 @@
                                         even as we update the project.</p>
                                 </li>
                             </div>
-                            <div
-                                class="p-4 w-full bg-red-400 mb-5 rounded-lg sm:p-4 dark:bg-gray-800 dark:border-gray-700">
+                            <div class="p-4 w-full bg-red-400 mb-5 rounded-lg sm:p-4 dark:bg-gray-800 dark:border-gray-700">
                                 <li class="mb-10 ml-4">
-                                    <div
-                                        class="absolute w-3 h-3 bg-red-500 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                                    <div class="absolute w-3 h-3 bg-red-500 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
                                     </div>
-                                    <time
-                                        class="mb-1 text-sm font-normal leading-none text-white dark:text-gray-500">March
+                                    <time class="mb-1 text-sm font-normal leading-none text-white dark:text-gray-500">March
                                         2022</time>
                                     <h3 class="text-lg font-semibold text-gray-900 ">Marketing UI design in
                                         Figma
