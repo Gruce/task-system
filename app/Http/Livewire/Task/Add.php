c4b6eed62282b37156efb7e88088e191b88eb6c7
zal3 <zainabamjedlatef31@gmail.com>
2022-06-26 01:31:13 +0300

    add user

--- app/Http/Livewire/Task/Add.php
+++ app/Http/Livewire/Task/Add.php
@@ -4,19 +4,16 @@ namespace App\Http\Livewire\Task;
 
 use Livewire\Component;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
-use Livewire\WithFileUploads;
-use Livewire\WithPagination;
-use App\Models\Task;
-use App\Models\Project;
-
+use Livewire\{WithFileUploads , WithPagination};
+use App\Models\{Task , Project , Employee};
 
 class Add extends Component
 {
     use  LivewireAlert, WithFileUploads , WithPagination;
 
-    public $task , $search;
-    public $files = [];
-    protected $listeners = ['$refresh' ];
+    public $task , $search , $userId , $employee_id, $limitPerPage = 10 , $files = [];
+
+    protected $listeners = ['$refresh' , 'delete' ,'load-more' => 'loadMore'];
 
     protected $rules = [
         'task.title' => 'required',
@@ -40,6 +37,32 @@ class Add extends Component
         $this->validate();
         $task = Task::create($this->task);
 
+        if($this->task->employees()->wherePivot('employee_id' , $this->userId)->exists()){
+            $this->alert('error', __('ui.data_already_exists'), [
+                'position' => 'top',
+                'timer' => 3000,
+                'toast' => true,
+                'timerProgressBar' => true,
+                'width' => '400',
+            ]);
+            return;
+        }
+
+        $this->task->employees()->attach($this->userId);
+
+        if(!$this->task->project->employees()->wherePivot('employee_id' , $this->userId)->exists())
+            $this->task->project->employees()->attach($this->userId);
+
+        $this->emitSelf('$refresh');
+
+        $this->alert('success', __('ui.data_has_been_add_successfully'), [
+            'position' => 'top',
+            'timer' => 3000,
+            'toast' => true,
+            'timerProgressBar' => true,
+            'width' => '400',
+        ]);
+
         if (count($this->files) > 0)
             foreach ($this->files as $file) {
                 $new_file = $task->files()->create([
@@ -55,8 +78,40 @@ class Add extends Component
             'timerProgressBar' => true,
             'width' => '400',
         ]);
+
+
     }
 
+    public function confirmed($id, $function){
+        $this->employee_id = $id;
+        $this->confirm(__('ui.are_you_sure'), [
+            'toast' => false,
+            'position' => 'center',
+            'showConfirmButton' => "true",
+            'cancelButtonText' => (__('ui.cancel')),
+            'confirmButtonText' => (__('ui.confirm')),
+            'onConfirmed' => $function,
+        ]);
+    }
+
+    public function delete(){
+        $this->task->employees()
+                        ->wherePivot('employee_id' , $this->employee_id)
+                        ->detach();
+
+        $this->emitSelf('$refresh');
+
+        $this->alert('success', __('ui.data_has_been_deleted_successfully'), [
+            'position' => 'top',
+            'timer' => 3000,
+            'toast' => true,
+            'timerProgressBar' => true,
+            'width' => '400',
+        ]);
+    }
+    public function loadMore(){
+        $this->limitPerPage = $this->limitPerPage + 10;
+    }
 
     public function render()
     {
@@ -65,8 +120,18 @@ class Add extends Component
             $search = '%' . $this->search . '%';
             $projects = Project::where('title' , 'LIKE' , $search)->paginate(24);
         }
+
+        $task_employees = [] ;
+        $employees = [];
+        if($this->search){
+            $search = '%' . $this->search . '%';
+            $employees = Employee::whereRelation('user' , 'name' , 'LIKE' , $search)->paginate(10);
+            $task_employees = $this->task->employees()->paginate($this->limitPerPage);
+    }
         return view('livewire.task.add' , [
             'projects' => $projects,
+            'employees' => $employees,
+            'task_employees' => $task_employees,
         ]);
     }
 }
--- resources/views/livewire/task/add.blade.php
+++ resources/views/livewire/task/add.blade.php
@@ -112,6 +112,52 @@
                             <option value="4">{{ __('ui.importance_very_high') }}</option>
                         </select>
                     </div>
+
+                    <div  class="flex flex-col">
+                        <div class="flex flex-col w-full gap-2 overflow-y-auto text-sm">
+                            <div class="flex justify-between w-full px-4 py-2 rounded-lg text-secondary-500">
+                                <div class="flex w-full gap-2">
+                                    <input wire:model="search" type="text" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500" placeholder="{{__('ui.name')}}" required>
+                                    @if ($search)
+                                        <select wire:model="userId" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                                            <option value="" selected>{{__('ui.select_employee')}}</option>
+                                            @foreach ($employees as $employee)
+                                                <option value="{{$employee->id}}">{{$employee->name}}</option>
+                                            @endforeach
+                                        </select>
+                                        <button wire:click="add" @click="add=!add" class="px-4 py-1 duration-150 ease-in-out delay-75 border rounded-lg hover:text-success-800 hover:bg-success-100">
+                                            <i class="fas fa-check"></i>
+                                        </button>
+                                    @endif
+                                </div>
+                            </div>
+
+                            {{-- Loop Item Below --}}
+                            @forelse ($task_employees as $employee)
+                            <div class="flex justify-between w-full px-4 py-2 rounded-lg hover:bg-secondary-50 text-secondary-500">
+                                <div class="flex items-center gap-4">
+                                    <img class="w-10 h-10 rounded-lg" src="{{$employee->photo}}" alt="Bordered avatar">
+                                    <div class="flex flex-col">
+                                        <span class="text-base font-normal text-secondary-700">
+                                            {{$employee->name}}
+                                        </span>
+                                        <span class="text-xs font-normal text-secondary-400">
+                                            {{-- {{$employee->pivot->created_at->diffForHumans()}} --}}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="flex gap-2">
+                                    <button wire:click="confirmed({{ $employee->id }} , 'delete')" class="px-4 py-1 duration-150 ease-in-out delay-75 rounded-lg hover:text-error-600 hover:bg-error-100">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            @empty
+                                {{__('ui.no_data')}}
+                            @endforelse
+                        </div>
+                    </div>
+
                     {{-- <div>
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             {{ __('ui.estimated_time') }}
