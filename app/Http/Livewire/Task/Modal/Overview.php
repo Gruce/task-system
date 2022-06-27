bbb430ded1ac9f774760f7963694397ef554dc41
zal3 <zainabamjedlatef31@gmail.com>
2022-06-27 22:32:35 +0300

    Update Overview.php

--- app/Http/Livewire/Task/Modal/Overview.php
+++ app/Http/Livewire/Task/Modal/Overview.php
@@ -4,11 +4,12 @@ namespace App\Http\Livewire\Task\Modal;
 
 use Livewire\Component;
 use App\Models\Project;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\WithPagination;
 
 class Overview extends Component
 {
-    use WithPagination;
+    use WithPagination ,LivewireAlert;
 
     protected $listeners = ['$refresh'];
 
@@ -19,6 +20,19 @@ class Overview extends Component
         'task.project_id' => 'required',
         'task.description' => 'required',
     ];
+
+    public function edit(){
+        $this->task->save();
+        $this->alert('success', __('ui.data_has_been_edited_successfully'), [
+            'position' => 'top',
+            'timer' => 3000,
+            'toast' => true,
+            'timerProgressBar' => true,
+            'width' => '400',
+        ]);
+        $this->emitTo( 'task.all' , '$refresh');
+    }
+
 
     public function render()
     {
