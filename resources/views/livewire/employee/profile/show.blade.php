1471c5dfab9d9ca764d4d6ba47305bd43014eb75
hassan_000 <salem.cf11@gmail.com>
2022-07-08 15:48:33 +0300

    zx

--- resources/views/livewire/employee/profile/show.blade.php
+++ resources/views/livewire/employee/profile/show.blade.php
@@ -144,7 +144,7 @@
                         </label>
                     </div>
                     <div class="mt-4">
-                        <button wire:keydown.enter="edit" wire:click="edit" @click="edit = !edit" type="button" class="text-white bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">{{__('ui.save')}}</button>
+                        <button wire:keydown.enter="edit" wire:click="edit" @click="edit = !edit" type="button" class="text-white bg-blue-700  font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">{{__('ui.save')}}</button>
                         <button @click="edit = !edit" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">{{__('ui.cancel')}}</button>
                     </div>
                 </div>
--- resources/views/livewire/project/card.blade.php
+++ resources/views/livewire/project/card.blade.php
@@ -38,6 +38,11 @@
                             </a>
                         </li>
                         <li>
+                            <a wire:click="confirmed({{ $project->id }})" href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-black">
+                                Export
+                            </a>
+                        </li>
+                        <li>
                             <a wire:click="confirmed({{ $project->id }})" href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-black">
                                 {{__('ui.delete')}}
                             </a>
