de30195a4d190836b06bf133a5334ef6a32c5388
hassan_000 <salem.cf11@gmail.com>
2022-07-09 00:40:04 +0300

    z

--- resources/views/livewire/project/card.blade.php
+++ resources/views/livewire/project/card.blade.php
@@ -38,8 +38,8 @@
                             </a>
                         </li>
                         <li>
-                            <a wire:click="confirmed({{ $project->id }})" href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-black">
-                                Export
+                            <a wire:click="" href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-black">
+                                Export to Excle
                             </a>
                         </li>
                         <li>
--- resources/views/livewire/project/main.blade.php
+++ resources/views/livewire/project/main.blade.php
@@ -1,14 +1,20 @@
 @section('title', __('ui.projects'))
 
-<div x-cloak class="p-4 sm:p-8" x-data="{ add: false }">
-    <div class="flex items-center justify-between mb-4">
-        <div>
-            <button @click="add = !add" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center gap-4">
-                <i class="ri-add-line"></i>
-                {{__('ui.add')}}
-            </button>
-        </div>
-        <div>
+<div x-cloak class="p-4 sm:p-8" x-data="{ add: false , export: false}">
+    <div class="flex items-center mb-4">
+            <div class=" basis-[10%]">
+                <button @click="add = !add" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center gap-4">
+                    <i class="ri-add-line"></i>
+                    {{__('ui.add')}}
+                </button>
+            </div>
+            <div class=" basis-[10%]">
+                <button  type="button" class="text-white bg-gradient-to-r from-gray-500 via-gray-600 to-gray-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-gray-300 shadow-lg shadow-gray-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center gap-4">
+                    <i class="fa-solid fa-file-arrow-down"></i>
+                    Export
+                </button>
+            </div>
+        <div class="basis-[80%]">
             Filteration
         </div>
     </div>
