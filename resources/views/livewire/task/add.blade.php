83c99ef43b8259f20430da8ca50db75d13f1f3c9
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-07-16 23:00:29 +0300

    add

--- resources/views/livewire/task/add.blade.php
+++ resources/views/livewire/task/add.blade.php
@@ -6,12 +6,13 @@
                     {{ __('ui.basicinfo') }}
                 </label>
                 <div class="grid xl:grid-cols-2 xl:gap-6">
-                    <div>
+                    <div class="@if($project) w-full @endif">
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             {{ __('ui.title') }}
                         </label>
                         <input wire:model.defer="task.title" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder={{ __('ui.title') }} required>
                     </div>
+                    @if (!$project)
                     <div>
                         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
                             {{ __('ui.choose_project') }}</label>
@@ -28,32 +29,7 @@
                             @endif
                         </div>
                     </div>
-                    {{-- <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                            {{ __('ui.start_date') }}
-                        </label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
-                                </svg>
-                            </div>
-                            <input datepicker="" wire:model.defer="start_at" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="Select date">
-                        </div>
-                    </div> --}}
-                    {{-- <div>
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                            {{ __('ui.end_date') }}
-                        </label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
-                                </svg>
-                            </div>
-                            <input datepicker="" wire:model.defer="end_at" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="Select date">
-                        </div>
-                    </div> --}}
+                    @endif
                     <div>
                         <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             {{ __('ui.start_date') }}
