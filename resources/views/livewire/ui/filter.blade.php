da1310712393b53fb3b970c9496ef1328d7ca29f
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-07-19 19:11:58 +0300

    add

--- resources/views/livewire/ui/filter.blade.php
+++ resources/views/livewire/ui/filter.blade.php
@@ -1,6 +1,28 @@
 <div>
-    <button id="dropdownRadioBgHoverButton" data-dropdown-toggle="dropdownRadioBgHover" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown radio <svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-        </svg></button>
+    <button id="filterHome" data-dropdown-toggle="dropdownFilterHome" data-dropdown-placement="left" class="p-2 py-1 duration-150 ease-in delay-75 rounded-lg  text-secondary-100">
+        <i class="text-xl fa-solid fa-filter"></i>
+    </button>
 
+    <div id="dropdownFilterHome" class="hidden mt-8 rounded-lg z-10 w-44 bg-secondary-50  divide-y divide-gray-100 shadow ">
+        <ul class="p-3 space-y-1 text-sm text-gray-700" aria-labelledby="filterHome">
+            <li>
+                <div class="flex items-center p-2 rounded hover:bg-white">
+                    <input id="default-radio-4" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                    <label for="default-radio-4" class="mx-2 w-full text-sm font-medium text-gray-900 rounded ">{{__('ui.current_week')}}</label>
+                </div>
+            </li>
+            <li>
+                <div class="flex items-center p-2 rounded hover:bg-white">
+                    <input id="default-radio-5" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                    <label for="default-radio-5" class="mx-2 w-full text-sm font-medium text-gray-900 rounded">{{__('ui.current_month')}}</label>
+                </div>
+            </li>
+            <li>
+                <div class="flex items-center p-2 rounded hover:bg-white">
+                    <input id="default-radio-6" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                    <label for="default-radio-6" class="mx-2 w-full text-sm font-medium text-gray-900 rounded">Default radio</label>
+                </div>
+            </li>
+        </ul>
+    </div>
 </div>
