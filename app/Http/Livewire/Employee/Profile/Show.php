aa8ec5d73111942fbc1af168ebf287cfe42739b5
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-07-22 03:28:26 +0300

    add

--- app/Http/Livewire/Employee/Profile/Show.php
+++ app/Http/Livewire/Employee/Profile/Show.php
@@ -23,23 +23,14 @@ class Show extends Component
         $this->taskID = $task_id;
     }
 
-    protected $rules = [
-        'employee.user.name' => 'required',
-        'employee.user.username' => 'required|unique:users,username',
-        'employee.user.password' => 'min:6',
-        'password_confirmation' => 'min:6|same:employee.user.password',
-        'employee.job' => 'required',
-        'employee.user.gender' => 'required',
-        'employee.user.phonenumber' => 'required',
-        'employee.user.email' => 'required|email|unique:users,email',
-    ];
+
 
     public function mount($id)
     {
         $this->employee = Employee::with(['tasks', 'files', 'user'])
             ->withCount(['tasks', 'files', 'projects'])
             ->findOrFail($id);
-        // $this->employee->user->password = null;
+
         $this->tasks = $this->employee->tasks()->get();
 
         $this->todo_tasks = $this->tasks->where('state', 1)->count();
@@ -93,34 +84,6 @@ class Show extends Component
         $this->employee->user->save();
     }
 
-
-    public function edit()
-    {
-
-        $this->employee->user->update([
-            'name' => $this->employee['user']['name'],
-            'username' => $this->employee['user']['username'],
-            'email' => $this->employee['user']['email'],
-            'phonenumber' => $this->employee['user']['phonenumber'],
-            // 'password' => $this->employee->user->password,
-        ]);
-
-        // $this->employee->job = $this->employee['job'];
-        // $this->employee->save();
-
-        $this->employee->update([
-            'job' => $this->employee['job'],
-        ]);
-
-
-        $this->alert('success', __('ui.data_has_been_edited_successfully'), [
-            'position' => 'top',
-            'timer' => 3000,
-            'toast' => true,
-            'timerProgressBar' => true,
-            'width' => '400',
-        ]);
-    }
 
     public function getPieChartModelProperty()
     {
