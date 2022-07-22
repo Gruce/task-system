e6f581fc6091ce50dae988c31069555f83b22347
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-07-23 00:36:34 +0300

    add

--- resources/views/livewire/task/all.blade.php
+++ resources/views/livewire/task/all.blade.php
@@ -1,5 +1,5 @@
 <div>
-    
+
     <div class="flex flex-col gap-4 sm:flex-row">
         <div class="flex flex-col gap-2 bg-white rounded-lg basis-1/3">
             <div class="flex items-center gap-2 p-3 m-1 text-lg rounded-lg text-secondary-600 bg-secondary-50">
@@ -9,15 +9,15 @@
 
             <div id="todo-1" class="flex flex-col gap-4 px-2 m-1 overflow-y-auto drag {{$project ? 'sm:h-minitasklist h-tasklist' : 'h-tasklist'}}" ondrop="drop(event)" ondragover="allowDrop(event)">
                 @forelse ($tasks as $item )
-                    @if($item->state == 1)
-                        @livewire('task.card' , ['task' => $item] , key($item->id . "-" . now()))
-                    @endif
+                @if($item->state == 1)
+                @livewire('task.card' , ['task' => $item] , key($item->id . "-" . now()))
+                @endif
                 @empty
-                    <div class="flex items-center justify-center">
-                        <div class="text-center">
-                            <h1 class="text-lg text-center text-gray-500">{{ __('ui.no_data') }}</h1>
-                        </div>
+                <div class="flex items-center justify-center">
+                    <div class="text-center">
+                        <h1 class="text-lg text-center text-gray-500">{{ __('ui.no_data') }}</h1>
                     </div>
+                </div>
                 @endforelse
             </div>
         </div>
@@ -29,18 +29,20 @@
 
             <div id="progress-2" class="flex flex-col gap-4 px-2 m-1 overflow-y-auto drag {{$project ? 'sm:h-minitasklist h-tasklist' : 'h-tasklist'}}" ondrop="drop(event)" ondragover="allowDrop(event)">
                 @forelse ( $tasks as $item)
-                    @if($item->state == 2)
-                        @livewire('task.card', ['task'=>$item], key('task-id-' . $item->id))
-                    @endif
+                @if($item->state == 2)
+                @livewire('task.card', ['task'=>$item], key('task-id-' . $item->id))
+                @endif
                 @empty
-                    <div class="flex items-center justify-center">
-                        <div class="text-center">
-                            <h1 class="text-lg text-center text-gray-500">{{ __('ui.no_data') }}</h1>
-                        </div>
+                <div class="flex items-center justify-center">
+                    <div class="text-center">
+                        <h1 class="text-lg text-center text-gray-500">{{ __('ui.no_data') }}</h1>
                     </div>
+                </div>
                 @endforelse
             </div>
+
         </div>
+
         <div class="flex flex-col gap-2 bg-white rounded-lg basis-1/3">
             <div class="flex items-center gap-2 p-3 m-1 text-lg rounded-lg text-secondary-600 bg-secondary-50">
                 <span class="w-3.5 h-3.5 bg-success-700 border-2 border-white rounded-full"></span>
@@ -49,18 +51,22 @@
 
             <div id="done-3" class="flex flex-col gap-4 px-2 m-1 overflow-y-auto drag {{$project ? 'sm:h-minitasklist h-tasklist' : 'h-tasklist'}}" ondrop="drop(event)" ondragover="allowDrop(event)">
                 @forelse ($tasks as $item )
-                    @if($item->state == 3)
-                        @livewire('task.card', ['task'=>$item], key('task-id-' . $item->id))
-                    @endif
+                @if($item->state == 3)
+                @livewire('task.card', ['task'=>$item], key('task-id-' . $item->id))
+                @endif
                 @empty
-                    <div class="flex items-center justify-center">
-                        <div class="text-center">
-                            <h1 class="text-lg text-center text-gray-500">{{ __('ui.no_data') }}</h1>
-                        </div>
+                <div class="flex items-center justify-center">
+                    <div class="text-center">
+                        <h1 class="text-lg text-center text-gray-500">{{ __('ui.no_data') }}</h1>
                     </div>
+                </div>
                 @endforelse
             </div>
         </div>
+
+    </div>
+    <div class="grid gap-1 m-2 lg:grid-cols-1 sm:grid-cols-1 pt-3" dir="ltr">
+        {{ $tasks->links() }}
     </div>
 
     <script>
