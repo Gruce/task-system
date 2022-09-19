143b4c267e9a9017209b44465be016347e610b11
Hassan Kadhim <66673458+HassanKadhim@users.noreply.github.com>
2022-09-19 20:10:31 +0300

    Update FilterProjects.php

--- app/Http/Livewire/Ui/FilterProjects.php
+++ app/Http/Livewire/Ui/FilterProjects.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 class FilterProjects extends Component
 {
     public $done = null;
+
     public function render()
     {
         $this->emit('filterProjects', $this->done);
